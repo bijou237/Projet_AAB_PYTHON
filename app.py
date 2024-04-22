c48a9d74ab90ebb264fa859f085fe820359d105d
bijou <bijoueyong.com>
2024-04-21 21:17:16 -0400

    dropdown list

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 from flask import Flask, render_template, request, url_for
 
 app= Flask(__name__)
+app.secret_key= "cle secret"
 
 @app.route('/')
+def home():
+    return render_template("index.html")
 
-def index():
-    return render_template('index.html')
+@app.route('/about')
+def about():
+    return render_template("about.html")
 
+@app.route('/evenements')
+def evenements():
+    return render_template("evenements.html")
 
-#@app.route('/about')
+@app.route('/form_events')
+def form_events():
+    return render_template("form_events.html")
