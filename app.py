dbe56b90b60dfabaf5d2db295026dfb41bd40ad7
bijou <bijoueyong.com>
2024-04-25 20:30:43 -0400

    formulaires updates

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, url_for,redirect,session
 from reservations.reservation_dao import ReservationDao
+from utilisateurs.utilisateur_dao import UtilisateurDao
+from paies.paie_dao import PaieDao
 
 app= Flask(__name__)
 app.secret_key= "cle secret"
@@ -37,23 +39,97 @@ def formulaire_Con():
 
 @app.route("/ajouter_reservation", methods=['POST', 'GET'])
 def ajouter_reservation():
-        req = request.form
-        print(req)
         message = None
-        employe = None
-        print("Methode.utilisee:" , request.method)
+        insertion = None
         if request.method == "POST": 
-            nom_event = req['film']
-            nom_util = req['nom_util']
-            placeD = req['place']
-            if nom_event == 'Selectionnez Votre Films' or nom_util == '' or placeD == '':
+            nom_event = request.form.get('film')
+            nom_util = request.form.get('nom_util')
+            place = request.form.get('place')
+            if  not nom_event or not nom_util or not place:
                 message = "error"
             else:
                 #employe = employe(nom_event, nom_util, placeD)
                 ajouter = ReservationDao()
-                message = ajouter.ajouter_reservation(nom_event, nom_util, placeD)
-                print(message)
-        return render_template("form.html", message=message, employe=employe)
+                insertion = ajouter.ajouter_reservation(nom_event, nom_util, place)
+                message = "succes"
+               # print(message)
+        return render_template("form.html", message=message, insertion =insertion)
+
+
+@app.route("/ajouter_reservation_concert", methods=['POST', 'GET'])
+def ajouter_reservation_concert():
+        message = None
+        insertion = None
+        if request.method == "POST": 
+            nom_event = request.form.get('film')
+            nom_util = request.form.get('nom_util')
+            place = request.form.get('place')
+            if  not nom_event or not nom_util or not place:
+                message = "error"
+            else:
+                #employe = employe(nom_event, nom_util, placeD)
+                ajouter = ReservationDao()
+                insertion = ajouter.ajouter_reservation(nom_event, nom_util, place)
+                message = "succes"
+               # print(message)
+        return render_template("formulaire_con.html", message=message, insertion =insertion)
+
+@app.route("/ajouter_reservation_diner", methods=['POST', 'GET'])
+def ajouter_reservation_diner():
+        message = None
+        insertion = None
+        if request.method == "POST": 
+            nom_event = request.form.get('film')
+            nom_util = request.form.get('nom_util')
+            place = request.form.get('place')
+            if  not nom_event or not nom_util or not place:
+                message = "error"
+            else:
+                #employe = employe(nom_event, nom_util, placeD)
+                ajouter = ReservationDao()
+                insertion = ajouter.ajouter_reservation(nom_event, nom_util, place)
+                message = "succes"
+               # print(message)
+        return render_template("formulaire_diner.html", message=message, insertion =insertion)
+
+@app.route("/ajouter_utilisateur", methods=['POST', 'GET'])
+def ajouter_utilisateur():
+        message = None
+        insertion = None
+        if request.method == "POST": 
+            nom = request.form.get('Nom')
+            prenom = request.form.get('Prenom')
+            age = request.form.get('age')
+            email = request.form.get('Email')
+            password = request.form.get('Password')
+            role = request.form.get('role')
+            if  not nom or not prenom or not age or not email or not password or not role:
+                message = "error"
+            else:
+                ajouter = UtilisateurDao()
+                insertion = ajouter.add_utilisateur(nom, prenom, age, email, password, role)
+                message = "succes"
+    
+        return render_template("formulaire_enreg.html", message=message, insertion =insertion)
+
+@app.route("/paiement", methods=['POST', 'GET'])
+def payer_evenement():
+        message = None
+        add=None
+        if request.method == "POST":  
+            Nomprenom = request.form.get('nom')
+            Numerocarte = request.form.get('Numero')
+            dateexp= request.form.get('date')
+            cvv = request.form.get('CVV')
+           
+            if  not Nomprenom or not Numerocarte or not dateexp or not cvv:
+                message = "error"
+            else:  
+
+                ajouter = PaieDao()
+                add = ajouter.payer_evenement(Nomprenom, Numerocarte, dateexp, cvv)
+                message = "succes"
+        return render_template("paie.html", message=message, add=add)
     
 @app.route('/login')
 def login():
@@ -61,7 +137,7 @@ def login():
 
 @app.route('/enregistrement')
 def enregistrement():
-    return render_template("ajout_util.html")
+    return render_template("formulaire_enreg.html")
 
 @app.route('/paiement')
 def paiement():
